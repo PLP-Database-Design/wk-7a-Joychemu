555e2bd0b473ab9b119a2d9a6fdfb05a8fcda2b0
Joychemu <64799702+Joychemu@users.noreply.github.com>
2025-05-05 09:33:30 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,20 +1,18 @@
--- Question 1
-CREATE TABLE CustomerOrder (
-    OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(100)
-);
-
-CREATE TABLE Product (
-    ProductID INT AUTO_INCREMENT PRIMARY KEY,
-    ProductName VARCHAR(100) UNIQUE
-);
-
-CREATE TABLE OrderProduct (
+ --  Question 1
+CREATE TABLE ProductDetail (
     OrderID INT,
-    ProductID INT,
-    FOREIGN KEY (OrderID) REFERENCES CustomerOrder(OrderID),
-    FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
 );
+
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 -- Question 2
 CREATE TABLE Orders (
@@ -22,11 +20,26 @@ CREATE TABLE Orders (
     CustomerName VARCHAR(100)
 );
 
-CREATE TABLE OrderItems (
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+CREATE TABLE Product (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
+
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
 
